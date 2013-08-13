6429dd825682005a4314e0874ed2d634a7b33fbb
Olaf <olaf_stellaard@msn.com>
2013-08-13 16:52:55 +0200

    Header 70%

--- index.php
+++ index.php
@@ -14,21 +14,21 @@
 			<div id="header">
 				<!-- Logo -->
 				<div id="logo">
-				
+					<img src="afbeeldingen/Psychologenpraktijk_Molenwaard_contouren.png" alt="Het logo van Psychologen Molenwaard"/>
 				</div>
 				
 				<!-- Navigatie -->
 				<div id="navigatie">
 					
 					<!-- NavButtons -->
-					<div id="NavButton">Welkom </div>
-					<div id="NavButton">Behandelaanbod </div>
-					<div id="NavButton">Aanmeldproblematiek </div>
-					<div id="NavButton">Tarieven en Annuleren </div>
-					<div id="NavButton">Aanmelden </div>
-					<div id="NavButton">Kennismakne </div>
-					<div id="NavButton">Contact </div>
-					<div id="NavButton">Formulieren </div>
+					<div id="NavButton"><span>Welkom </span></div>
+					<div id="NavButton"><span>Behandelaanbod </span></div>
+					<!--<div id="NavButton"><span>Aanmeld-<br>problematiek </span></div>-->
+					<div id="NavButton"><span>Tarieven </span></div>
+					<div id="NavButton"><span>Aanmelden </span></div>
+					<div id="NavButton"><span>Kennismaken </span></div>
+					<div id="NavButton"><span>Contact </span></div>
+					<div id="NavButton"><span>Formulieren </span></div>
 				</div>
 				
 			</div>
