0f20bb547505d25cbfa551d5e4a991fba437e782
Olaf <olaf_stellaard@msn.com>
2013-08-16 22:35:52 +0200

    footer toegevoegd

--- index.php
+++ index.php
@@ -23,7 +23,6 @@
 					<!-- NavButtons -->
 					<a href="index.php?p=welkom"><div id="NavButton"><span>Welkom </span></div></a>
 					<a href="index.php?p=behandelaanbod"><div id="NavButton"><span>Behandelaanbod </span></div></a>
-					<!--<a href="index.php?p=aanmeldproblematiek"><div id="NavButton"><span>Aanmeld-<br>problematiek </span></div></a>-->
 					<a href="index.php?p=tarieven"><div id="NavButton"><span>Tarieven </span></div></a>
 					<a href="index.php?p=aanmelden"><div id="NavButton"><span>Aanmelden </span></div></a>
 					<a href="index.php?p=kennismaken"><div id="NavButton"><span>Kennismaken </span></div></a>
@@ -73,7 +72,17 @@
 			
 			<!-- Footer -->
 			<div id="footer">
-				
+				<img src="afbeeldingel/ppmolenwaard_logo_klein.jpg" alt="Psychologenpraktijk Molenwaard logo" class="float"></img>
+				<img src="afbeeldingel/nip-logo.png" alt="NIP logo" class="float"></img>
+				<div class="float">
+				<p>Psychologenpraktijk Molenwaard</p>
+				<p>Graafland 31</p>
+				<p>2964 BJ, Groot-Ammers</p>
+				<p>Telefoonnummer: 06-23563981</p>
+				</div>
+				<img src="afbeeldingel/cvppp.png" alt="CVPPP logo" class="float"></img>
+				<img src="afbeeldingel/facebook.png" alt="Facebook logo" class="float"></img>
+			
 			</div>
 			
 		</div>
