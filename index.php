2679cf851de1deab89962b3067e0c10ae841e201
Olaf <olaf_stellaard@msn.com>
2013-08-16 20:47:31 +0200

    Navigatie toegevoegd

--- index.php
+++ index.php
@@ -21,14 +21,14 @@
 				<div id="navigatie">
 					
 					<!-- NavButtons -->
-					<div id="NavButton"><span>Welkom </span></div>
-					<div id="NavButton"><span>Behandelaanbod </span></div>
-					<!--<div id="NavButton"><span>Aanmeld-<br>problematiek </span></div>-->
-					<div id="NavButton"><span>Tarieven </span></div>
-					<div id="NavButton"><span>Aanmelden </span></div>
-					<div id="NavButton"><span>Kennismaken </span></div>
-					<div id="NavButton"><span>Contact </span></div>
-					<div id="NavButton"><span>Formulieren </span></div>
+					<a href="index.php?p=welkom"><div id="NavButton"><span>Welkom </span></div></a>
+					<a href="index.php?p=behandelaanbod"><div id="NavButton"><span>Behandelaanbod </span></div></a>
+					<!--<a href="index.php?p=aanmeldproblematiek"><div id="NavButton"><span>Aanmeld-<br>problematiek </span></div></a>-->
+					<a href="index.php?p=tarieven"><div id="NavButton"><span>Tarieven </span></div></a>
+					<a href="index.php?p=aanmelden"><div id="NavButton"><span>Aanmelden </span></div></a>
+					<a href="index.php?p=kennismaken"><div id="NavButton"><span>Kennismaken </span></div></a>
+					<a href="index.php?p=contact"><div id="NavButton"><span>Contact </span></div></a>
+					<a href="index.php?p=formulieren"><div id="NavButton"><span>Formulieren </span></div></a>
 				</div>
 				
 			</div>
@@ -37,6 +37,37 @@
 			<div id="content">
 				<?php
 				//Laad content file
+				$pagina = $_GET['p'];
+				switch($pagina)
+				{
+					case "welkom":
+						include("welkom.html");
+						break;
+					case "aanmelden":
+						include("aanmelden.html");
+						break;
+					case "aanmeldproblematiek":
+						include("aanmeldproblematiek.html");
+						break;
+					case "behandelaanbod":
+						include("behandelaanbod.html");
+						break;
+					case "contact":
+						include("contact.html");
+						break;
+					case "formulieren":
+						include("formulieren.html");
+						break;			
+					case "kennismaken":
+						include("kennismaken.html");
+						break;
+					case "tarieven":
+						include("tarieven.html");
+						break;
+					default:
+						include("welkom.html");
+						break;
+				}
 				?>
 			</div>
 			
